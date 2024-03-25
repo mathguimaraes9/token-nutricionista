05d60d55a3f05c70e2d7b06a91f71d22f9997b7e
Matheus <matheushfguimaraes@gmail.com>
2024-03-25 18:47:26 -0300

    implementando token

--- src/app.js
+++ src/app.js
@@ -12,6 +12,51 @@ const clientes = [
   { id: 2, nome: "Rafael Bohland", cpf: "557.045.430-65" },
 ];
 
+const tokens = [];
+
+// Criar um novo token de um certo cliente
+app.post("/api/token", (req, res) => {
+  const { clienteId } = req.body;
+
+  // Verificação se o cliente existe
+  const cliente = clientes.find((c) => c.id === clienteId);
+  if (!cliente) {
+    return res.status(404).json({ error: "Cliente não encontrado." });
+  }
+
+  const token = uuid.v4();
+
+  tokens.push({ token, clienteId });
+
+  return res.json({ token });
+});
+
+// Rota para verificar um token e obter os dados do cliente
+app.post("/api/verify", (req, res) => {
+  const { token } = req.body;
+
+  if (!token) {
+    return res.status(400).json({ error: "Token é obrigatório." });
+  }
+
+  const foundToken = tokens.find((t) => t.token === token);
+
+  if (!foundToken) {
+    return res.status(404).json({ error: "Token inválido." });
+  }
+
+  // Encontra o cliente correspondente ao token
+  const cliente = clientes.find((c) => c.id === foundToken.clienteId);
+
+  if (!cliente) {
+    return res
+      .status(404)
+      .json({ error: "Cliente não encontrado para o token fornecido." });
+  }
+
+  return res.json({ cliente });
+});
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando no endereço https://localhost:${PORT}`);
+  console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
 });
